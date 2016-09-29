27daa0cd56849b9cf3d2865cf306ff01157d264d
marony <marony@binbo-kodakusan.com>
2016-09-29 23:16:21 +0900

    problem 11, 12

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate time;
 
 fn main() {
     let solvers = [
-        problem_001, problem_002, problem_003, problem_004, problem_005,
-        problem_006, problem_007, problem_008, problem_009, problem_010];
+        /*problem_001, problem_002, problem_003, problem_004, problem_005,
+        problem_006, problem_007, problem_008, problem_009, problem_010,*/
+        problem_011, problem_012];
     for s in &solvers {
         let now = time::now();
         s();
@@ -276,3 +277,125 @@ fn problem_010() {
     }
 }
 
+fn problem_011() {
+    println!("problem_011");
+    const LEN: usize = 4;
+    const SLEN: i32 = 20;
+    let ss;
+    {
+        let s = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 \
+            49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 \
+            81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 \
+            52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 \
+            22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 \
+            24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 \
+            32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 \
+            67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 \
+            24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 \
+            21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 \
+            78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 \
+            16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 \
+            86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 \
+            19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 \
+            04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 \
+            88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 \
+            04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 \
+            20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 \
+            20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 \
+            01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+        ss = s.split_whitespace().map(|s| s.parse::<i32>().unwrap_or(0i32)).collect::<Vec<_>>();
+    }
+    fn get_nums<'a>(ss: &'a Vec<i32>, index: i32) -> Vec<Vec<i32>> {
+        let mut nss: Vec<Vec<i32>> = vec![];
+        let x = (index % SLEN) as usize;
+        let y = (index / SLEN) as usize;
+
+        // 右
+        let is = vec![(index + 0) as usize, (index + 1) as usize, (index + 2) as usize, (index + 3) as usize];
+        if is.iter().all(|&i| 0 <= i && i < (SLEN * SLEN) as usize && (i / SLEN as usize) == y) {
+            let mut ns = Vec::with_capacity(LEN);
+            for i in 0..LEN {
+                ns.push(ss[is[i]]);
+            }
+            nss.push(ns);
+        } else {
+            ()
+        }
+        // 下
+        let is = vec![(index + 0) as usize, (index + SLEN) as usize, (index + SLEN * 2) as usize, (index + SLEN * 3) as usize];
+        if is.iter().all(|&i| 0 <= i && i < (SLEN * SLEN) as usize && (i % SLEN as usize) == x) {
+            let mut ns = Vec::with_capacity(LEN);
+            for i in 0..LEN {
+                ns.push(ss[is[i]]);
+            }
+            nss.push(ns);
+        } else {
+            ()
+        }
+        // 右下
+        let is = vec![(index + 0) as usize, (index + SLEN + 1) as usize, (index + SLEN * 2 + 2) as usize, (index + SLEN * 3 + 3) as usize];
+        if is.iter().all(|&i| 0 <= i && i < (SLEN * SLEN) as usize && (i % SLEN as usize) >= x && (i / SLEN as usize) >= y) {
+            let mut ns = Vec::with_capacity(LEN);
+            for i in 0..LEN {
+                ns.push(ss[is[i]]);
+            }
+            nss.push(ns);
+        } else {
+            ()
+        }
+        // 左下
+        let is = vec![(index + 0) as usize, (index + SLEN - 1) as usize, (index + SLEN * 2 - 2) as usize, (index + SLEN * 3 - 3) as usize];
+        if is.iter().all(|&i| 0 <= i && i < (SLEN * SLEN) as usize && (i % SLEN as usize) <= x && (i / SLEN as usize) >= y) {
+            let mut ns = Vec::with_capacity(LEN);
+            for i in 0..LEN {
+                ns.push(ss[is[i]]);
+            }
+            nss.push(ns);
+        } else {
+            ()
+        }
+        nss
+    }
+    {
+        use std::cmp;
+
+        let mut a = 0;
+        for i in 0..SLEN * SLEN {
+            for ns in get_nums(&ss, i) {
+                a = cmp::max(a, ns.iter().fold(1, |acc, &n| acc * n));
+            }
+        }
+        println!("{}", a);
+    }
+}
+
+fn factors<'a>(n: u64) -> Vec<u64> {
+    let mut xs = vec![];
+    for i in 1u64..(n as f64).sqrt() as u64 + 1 {
+        if n % i == 0 {
+            xs.push(i);
+            xs.push(n / i);
+        }
+    }
+    xs
+}
+
+fn triangle_number(n: u64) -> u64 {
+    (1 + n) * n / 2
+}
+
+// Problem 12 - Project Euler <https://projecteuler.net/problem=12>
+// Highly divisible triangular number
+fn problem_012<'a>() {
+    println!("problem_012");
+    {
+        let x = 500;
+        let a = (1..).map(|n| {
+            let tri = triangle_number(n);
+            let fac = factors(tri);
+            let len = fac.len();
+            (tri, fac, len)
+        }).find(|xs| xs.2 > x).unwrap_or((0, vec![], 0));
+        println!("{:?}", &a);
+    }
+}
